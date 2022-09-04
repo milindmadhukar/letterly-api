28bb47f1af4b928597c9adf6912773daf4ebff36
Milind Madhukar <m.milind15@gmail.com>
2022-09-04 12:14:44 +0530

    changed map to array for players

--- models/player.go
+++ models/player.go
@@ -1,6 +1,7 @@
 package models
 
 type Player struct {
+  SessionID string `json:"sessionID"`
   UserName string `json:"userName"`
   Score int `json:"score"`
   IsPlaying bool `json:"isPlaying"`
--- utils/hop.go
+++ utils/hop.go
@@ -72,19 +72,19 @@ func ExecuteHopRequest(endpoint, reqMethod string, reqBody io.Reader, params map
 }
 
 func CreateHopChannel(hostSessionID, username string) (*hopChannel, error) {
-  player := models.Player{
-  	UserName:  username,
-  	Score:     0,
-  	IsPlaying: false,
-  }
 	reqBody, err := json.Marshal(map[string]interface{}{
 		"type": "unprotected",
 		"state": map[string]interface{}{
 			"game": "created",
 			"host": hostSessionID,
 			// Create a state players object as a map of sessionID to player name
-			"players": map[string]models.Player{
-				hostSessionID: player,
+			"players": []models.Player{
+        {
+          SessionID: hostSessionID,
+        	UserName:  username,
+        	Score:     0,
+        	IsPlaying: false,
+        },
 			},
       "playerCount": 1,
 		},
